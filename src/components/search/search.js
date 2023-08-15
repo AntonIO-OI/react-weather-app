80f2f1ca882219ddc0f95397d626121330ab2397
AntonIO-OI <anton.bur1337@gmail.com>
2023-08-15 20:54:35 +0300

    add search bar template

---
+++ src/components/search/search.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+    />
+  );
+};
+
+export default Search;
